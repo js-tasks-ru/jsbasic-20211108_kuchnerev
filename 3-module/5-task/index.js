695d8b42762c149e3965b51fd400c0d1fcb2417c
Vasiliy Kushnerev <kuchnerev@tut.by>
2021-11-28 15:38:49 +0300

    Функция getMinMax()

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,16 @@
 function getMinMax(str) {
-  // ваш код...
+   
+        let ourArray = str.split(' ').filter(item => !isNaN(item));
+        let sortArray = ourArray.sort((a, b) => a-b);
+        let maxArray = Math.max.apply(null,sortArray);
+        let minArray = Math.min.apply(null,sortArray);
+        let maxMinArray = sortArray.filter(item => item == maxArray || item == minArray );
+
+
+        let minValue = +maxMinArray[0];
+        let maxValue = +maxMinArray[1];
+        return{
+            min:minValue,
+            max:maxValue,
+        }
 }
