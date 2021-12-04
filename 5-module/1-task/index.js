42b3e4402a2c43e4f7e79c59ac1e9125ad79a01e
Vasiliy Kushnerev <kuchnerev@tut.by>
2021-12-04 23:54:18 +0300

    Создал функцию toggleText

--- 5-module/1-task/index.js
+++
@@ -1,4 +0,0 @@
-function hideSelf() {
-let button = document.querySelector('.hide-self-button');
-button.addEventListener('click', ()=> button.hidden = true);
-}
--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,7 @@
 function toggleText() {
-  // ваш код...
+  let button = document.querySelector('.toggle-text-button');
+  let target = document.getElementById('text');
+  button.addEventListener('click', function () {
+      target.hidden = !target.hidden;
+  });
 }
